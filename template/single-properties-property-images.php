bb28972fe6a42cd1cb7d05b26e58d403c216eec9
Jon Schroeder <jon@elod.in>
2021-04-15 01:36:37 -0500

    Minor

--- template/single-properties-property-images.php
+++ template/single-properties-property-images.php
@@ -14,6 +14,8 @@ function apartmentsync_single_property_images_yardi( $property_images ) {
     if ( $number_of_images <= 5 ) {
         
         echo '<div class="image-single">';
+        
+            $count = 1;
                     
             foreach( $property_images as $property_image ) {
                 
@@ -21,12 +23,27 @@ function apartmentsync_single_property_images_yardi( $property_images ) {
                 $url = $property_image->ImageURL;
                 $alt = $property_image->AltText;
                 
+                var_dump( $url );
+                
+                // detect if there are special characters
+                $regex = preg_match('[@_!#$%^&*()<>?/|}{~:]', $url);
+                                    
+                // bail on this slide if there are special characters in the image url
+                if ( $regex )
+                    break;
+                    
+                // bail if there is no image src
+                if ( !$url )
+                    break;
+                
                 printf( '<a data-fancybox="single-properties" href="%s"><img src="%s" alt="%s" title="%s" /></a>', $url, $url, $alt, $title );
+                
+                $count++;
                 
             }
         
             if ( $number_of_images > 1 )
-                printf( '<a data-fancybox-trigger="single-properties" class="viewall" href="#">View %s images</a>', $number_of_images );
+                printf( '<a data-fancybox-trigger="single-properties" class="viewall" href="#">View %s images</a>', $count );
         
         echo '</div>';
         
