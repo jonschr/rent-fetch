b19c3b709f3841a1c7783988608b57d32c50c2f2
Jon Schroeder <jon@elod.in>
2024-02-21 00:35:47 -0600

    Minor formatting stuff

--- lib/api/appfolio/appfolio-check-credentials.php
+++ lib/api/appfolio/appfolio-check-credentials.php
@@ -6,17 +6,17 @@
  * @return  bool  true if creds exist, false if not
  */
 function rentfetch_check_creds_appfolio() {
-    
-    $appfolio_integration_creds = get_field( 'appfolio_integration_creds', 'option' );
-    $appfolio_database_name = $appfolio_integration_creds['appfolio_database_name'];
-    $appfolio_client_id = $appfolio_integration_creds['appfolio_client_id'];
-    $appfolio_client_secret = $appfolio_integration_creds['appfolio_client_secret'];
-        
-    // return false if there's no api key set
-    if ( !$appfolio_database_name || !$appfolio_client_id || !$appfolio_client_secret )      
-        return false;
-    
-    // return true if there's an api key
-    return true;
-    
+	
+	$appfolio_integration_creds = get_field( 'appfolio_integration_creds', 'option' );
+	$appfolio_database_name = $appfolio_integration_creds['appfolio_database_name'];
+	$appfolio_client_id = $appfolio_integration_creds['appfolio_client_id'];
+	$appfolio_client_secret = $appfolio_integration_creds['appfolio_client_secret'];
+		
+	// return false if there's no api key set
+	if ( !$appfolio_database_name || !$appfolio_client_id || !$appfolio_client_secret )      
+		return false;
+	
+	// return true if there's an api key
+	return true;
+	
 }
